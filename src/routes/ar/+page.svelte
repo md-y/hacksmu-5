feb1b62e99b4cc25140be213ab9422b5905c921c
md-y <15069105+md-y@users.noreply.github.com>
2023-09-16 13:03:13 -0500

    Added sass

---
+++ src/routes/ar/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import three from 'three';
+	import threex from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only';
+</script>
+
+<canvas id="ar-canvas" />
+
+<style lang="scss">
+	#id-canvas {
+		background-color: black;
+		width: 100%;
+		height: 100%;
+	}
+</style>
