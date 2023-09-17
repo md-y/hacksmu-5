bd24d07ba1207054e1e24ab45eef0fef44a90fdd
jayeshpaluru <113400123+jayeshpaluru@users.noreply.github.com>
2023-09-16 20:27:20 -0500

    Update test.py

--- NaturalLanguageInterface/test.py
+++ NaturalLanguageInterface/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 
 # Load the dataset from the JSON file
-with open("/Users/jayeshpaluru/Downloads/HackSMU/NaturalLanguageInterface/dataset.json", "r") as file:
+with open("/Users/jayeshpaluru/Downloads/HackSMU/Dataset Creation/dataset.json", "r") as file:
     json_data = json.load(file)
 data_json = pd.DataFrame(json_data)
 
