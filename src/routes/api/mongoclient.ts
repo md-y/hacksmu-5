3f15faddc9ca871806a0ca622cec558e2ba8bf2d
md-y <15069105+md-y@users.noreply.github.com>
2023-09-16 20:24:30 -0500

    Make mongodb use cert

--- src/routes/api/mongoclient.ts
+++ src/routes/api/mongoclient.ts
@@ -1,19 +1,19 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-import 'dotenv/config';
+import { readFileSync } from 'node:fs';
 
 let clientCache: MongoClient;
 
 export async function getClient() {
 	if (clientCache) return clientCache;
 
-	const uri = `mongodb+srv://jayeshpaluru:${process.env.MONGODB_PASSWORD}@clustercbredata.o1vqwld.mongodb.net/?retryWrites=true&w=majority`;
+	const uri = `mongodb+srv://clustercbredata.o1vqwld.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority`;
+	const credentials = 'X509-cert-6706186085905149183.pem';
+	const readFile = readFileSync(credentials);
 
 	const client = new MongoClient(uri, {
-		serverApi: {
-			version: ServerApiVersion.v1,
-			strict: true,
-			deprecationErrors: true
-		}
+		serverApi: ServerApiVersion.v1,
+		cert: readFile,
+		key: readFile
 	});
 
 	await client.connect();
